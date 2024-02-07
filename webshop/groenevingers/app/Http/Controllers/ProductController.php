e3b278b1fdcbf69c79e4fa32f53869a7e27b8610
guynamedcurt <jaccoadriaan@gmail.com>
2024-02-07 15:24:15 +0100

    Product model en controller gemaakt

---
+++ webshop/groenevingers/app/Http/Controllers/ProductController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+
+        return View::make('products.index')
+            ->with('products', $products);
+    }
+    
+    public function show(string $id): View
+    {
+        return view('products.show', [
+            'products' => Product::findOrFail($id)
+        ]);
+    }
+
+    public function create()
+    {
+        return View::make('products.create');
+    }
+
+    public function update()
+    {
+        $rules = array(
+            'name' => 'required',
+            'categorie' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+        );
+        $validator = Validator::make(Input::all(), $rules);
+
+        if ($validator->fails()) {
+            return Redirect::to('products/' . $id . '/edit')
+                ->withErrors($validator)
+                ->withInput(Input::except('password'));
+        } else {
+            $product = product::find($id);
+            $product->name          = Input::get('name');
+            $product->categorie     = Input::get('categorie');
+            $product->description   = Input::get('description');
+            $product->price         = Input::get('price');
+            $product->save();
+
+            Session::flash('message', 'Artikel is succesvol geüpdate!');
+            return Redirect::to('products');
+        }
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $name = $request->name;
+        $categorie = $request->categorie;
+        $description = $request->description;
+        $price = $request->price;
+
+        return redirect('/products');
+    }
+
+    public function destroy($id)
+    {
+        $product = product::find($id);
+        $product->delete();
+
+        Session::flash('message', 'Succesvol het artikel verwijdert!');
+        return Redirect::to('products.index');
+    }
+}
