081539253aaef32e570cd83d18476a1a826f8941
Tomas.io <ps227258@SummaCollege.nl>
2024-02-26 13:43:21 +0100

    footer updated

---
+++ webshop/groenevingers/resources/views/includes/footer.blade.php
@@ -0,0 +1,23 @@
+<footer class="main-footer">
+    <div class="footer-column">
+        <div class="contact-list">
+            <h5>Contactgegevens</h5>
+            <div class="contact-information">
+                <span>06-12345678</span>
+                <span>Teststraat 5</span>
+                <span>4444 KA Nuenen</span>
+            </div>
+            
+            <span class="copyright">© 2024 GroeneVingers</span>
+        </div>
+
+        <div class="socials-list">
+            <h5>Socials</h5>
+            <a href="https://www.instagram.com/groenevingersgv/">Instagram</span>
+        </div>
+    </div>
+
+    <div class="footer-column">
+        <img src="{{ url('assets/images/Logo.png') }}" alt="logo" />
+    </div>
+</footer>
