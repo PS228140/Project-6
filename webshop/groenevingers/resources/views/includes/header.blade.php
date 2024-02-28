6e3b493e840a26567757d42a7f2fc406581c4e01
Tomas.io <ps227258@SummaCollege.nl>
2024-02-28 13:19:09 +0100

    added the authorisation to the website

--- webshop/groenevingers/resources/views/includes/header.blade.php
+++ webshop/groenevingers/resources/views/includes/header.blade.php
@@ -5,5 +5,6 @@
         <a class="nav-item" href="{{route('homepage.index')}}">Home</a>
         <a class="nav-item" href="{{route('products.index')}}">{{ GoogleTranslate::trans('Products', app()->getLocale()) }}</a>
         <a class="nav-item" href="{{route('contact.index')}}">{{ GoogleTranslate::trans('Contact', app()->getLocale()) }}</a>
+        <a class="nav-item" href="{{route('dashboard')}}">{{ GoogleTranslate::trans('Dashboard', app()->getLocale()) }}</a>
     </nav>
 </header>
