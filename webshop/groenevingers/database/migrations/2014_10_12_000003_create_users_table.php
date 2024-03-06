2bafcd4f47937c1d6e2a4601596c2b2b3e550e9e
Tomas.io <ps227258@SummaCollege.nl>
2024-03-06 12:37:23 +0100

    modified migrations

--- webshop/groenevingers/database/migrations/2014_10_12_000003_create_users_table.php
+++ webshop/groenevingers/database/migrations/2014_10_12_000003_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,20 +13,20 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->id();
-            $table->unsignedBigInteger('role')->index()->default(3);
+            $table->unsignedBigInteger('role_id')->index()->default(3);
             $table->string('email')->unique();
             $table->string('password');
             $table->string('name');
-            $table->unsignedBigInteger('branch')->index()->default(1);
-            $table->unsignedBigInteger('status')->index()->default(1);
+            $table->unsignedBigInteger('branch_id')->index()->default(1);
+            $table->unsignedBigInteger('status_id')->index()->default(1);
             $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
 
-            $table->foreign('role')->references('id')->on('roles')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('branch')->references('id')->on('branches')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('status')->references('id')->on('status')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('branch_id')->references('id')->on('branches')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('status_id')->references('id')->on('statuses')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
--- webshop/groenevingers/database/migrations/2024_02_28_131308_create_products_table.php
+++ webshop/groenevingers/database/migrations/2024_02_28_131308_create_products_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->engine = "InnoDB";
+            $table->engine = 'InnoDB';
             $table->id();
             $table->unsignedBigInteger('api_id');
             $table->unsignedBigInteger('categorie_id')->index()->default(1);
