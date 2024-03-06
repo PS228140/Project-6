57ad5346a39b1f11e58faa4ef7136ac784806ced
Tomas.io <ps227258@SummaCollege.nl>
2024-03-06 12:52:30 +0100

    adjusted some spacing

--- webshop/groenevingers/resources/views/user/index.blade.php
+++ webshop/groenevingers/resources/views/user/index.blade.php
@@ -8,11 +8,12 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+                <div class="p-6 text-gray-900 dark:text-gray-100 flex flex-col gap-2">
                     @foreach ($users as $user)
-                        <div class="p-2 grid grid-cols-3 grid-rows-1">
+                        <div class="grid grid-cols-4 grid-rows-1">
                             <span class="p-2">{{$user->name}}</span>
-                            <span class="p-2 text-center">{{$user->status->name}}</span>
+                            <span class="py-2 text-center">{{$user->status->name}}</span>
+                            <span class="py-2 text-center">{{$user->branch->name}}</span>
 
                             @if(Auth::user()->role->name === "Admin" || Auth::user()->role->name === "Manager")
                                 <button onclick="location.href='{{ route('users.show', ['user' => $user->id]) }}'" class="bg-blue-500 hover:bg-blue-700 justify-self-end p-2 w-24 rounded">
