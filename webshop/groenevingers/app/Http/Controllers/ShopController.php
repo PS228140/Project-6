18b79e8da6e5030707b7aacaf0366ca0a03826cb
Tomas.io <ps227258@SummaCollege.nl>
2024-03-14 08:51:19 +0100

    updated Controllers

---
+++ webshop/groenevingers/app/Http/Controllers/ShopController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ShopController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::paginate(15);
+
+        return view('shop.index', compact('products'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $product = Product::find($id);
+
+        return view('shop.show', ['product' => $product]);
+    }
+}
