62c9427fcd58cb1f783fa9442394760f4323dbc6
Tomas.io <ps227258@SummaCollege.nl>
2024-04-17 18:07:07 +0200

    minor cleanup

--- webshop/groenevingers/resources/views/management/create.blade.php
+++ webshop/groenevingers/resources/views/management/create.blade.php
@@ -33,7 +33,7 @@
 
                         <div class="mt-6">
                             <label for="quantity" class="block font-medium text-gray-700 dark:text-gray-300">Quantity</label>
-                            <input id="quantity" name="quantity" min="0" max="100" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full" type="number">
+                            <input id="quantity" name="quantity" min="0" max="100" value="1" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm w-full" type="number">
                         </div>
 
                         <button type="submit" class="mt-6 w-min inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Add</button>
