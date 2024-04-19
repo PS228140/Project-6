2a5a3ad8c689dfbbf9cfebd9181493194a7503ce
Tomas.io <ps227258@SummaCollege.nl>
2024-04-19 10:20:50 +0200

    added the databaseSeeders for the instagram embeds

--- webshop/groenevingers/database/seeders/DatabaseSeeder.php
+++ webshop/groenevingers/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
             UsersSeeder::class,
             CategoriesSeeder::class,
             ProductsSeeder::class,
+            DymanticProfileSeeder::class,
+            DymanticTokenSeeder::class,
         ]);
     }
 }
