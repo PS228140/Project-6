b7800192aa440c1bc85305b85d5c881fab9aa380
Tomas.io <ps227258@SummaCollege.nl>
2024-04-23 11:55:20 +0200

    updated comments

--- webshop/groenevingers/app/Models/Product.php
+++ webshop/groenevingers/app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Http;
 
 class Product extends Model
 {
@@ -35,5 +36,18 @@ class Product extends Model
     public function categorie(): BelongsTo
     {
         return $this->belongsTo(Categorie::class);
+    }
+
+    /**
+     * Get a specific product from the Kuin API
+     * 
+     * @param int $id id of the product to fetch
+     * @return $response array of product specs
+     */
+    public static function getProduct($id)
+    {
+        $response = Http::withToken("50|Oy8mM3g2A8jSTpiHoxrRXXdspGlvQHbbQ45qM272")->get("https://kuin.summaict.nl/api/product/" . $id);
+
+        return $response;
     }
 }
