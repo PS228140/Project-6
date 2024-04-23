a37376caf4148f30d8c27aeb62cd74b981ada7b8
Tomas.io <ps227258@SummaCollege.nl>
2024-04-23 11:48:11 +0200

    added options for placing an order at Kuin B.V.

--- webshop/groenevingers/app/Http/Controllers/ManagementController.php
+++ webshop/groenevingers/app/Http/Controllers/ManagementController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -23,12 +21,21 @@ class ManagementController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Request $request)
     {
         $products = Http::withToken("50|Oy8mM3g2A8jSTpiHoxrRXXdspGlvQHbbQ45qM272")->get("https://kuin.summaict.nl/api/product");
         $products = $products->json();
+        
+        if ($request->order_id !== null) {
+            $order = Http::withToken("50|Oy8mM3g2A8jSTpiHoxrRXXdspGlvQHbbQ45qM272")->get("https://kuin.summaict.nl/api/orderItem?order_id=" . $request->order_id);
+            $order = $order->json();
 
-        return view('management.create', ["products" => $products, "order_id" => null]);
+            $order_id = $order[0]["order_id"];
+
+            return view('management.create', ["products" => $products, "order_id" => $order_id, "orderrows" => $order]);
+        }
+
+        return view('management.create', ["products" => $products, "order_id" => null, "orderrows" => null]);
     }
 
     /**
@@ -36,13 +43,32 @@ class ManagementController extends Controller
      */
     public function store(Request $request)
     {
-        // if ($request->order_id) {
-        //     dd([$request->product, $request->quantity, $request->order_id]);
-        // } else {
-        //     dd([$request->product, $request->quantity]);
-        // }
+        if ($request->order_id) {
+            $response = Http::withToken("50|Oy8mM3g2A8jSTpiHoxrRXXdspGlvQHbbQ45qM272")->post("https://kuin.summaict.nl/api/orderItem?order_id=" . $request->order_id, [
+                'product_id' => $request->product,
+                'quantity' => $request->quantity
+            ]);
 
-        return redirect()->route('management.index');
+            if ($response->successful()) {
+                return redirect()->route('management.create', ['order_id' => $response["order_id"]]);
+            }
+        } else {
+            $response = Http::withToken("50|Oy8mM3g2A8jSTpiHoxrRXXdspGlvQHbbQ45qM272")->post("https://kuin.summaict.nl/api/orderItem", [
+                'product_id' => $request->product,
+                'quantity' => $request->quantity
+            ]);
+
+            if ($response->successful()) {
+                $order = $response->json();
+
+                $products = Http::withToken("50|Oy8mM3g2A8jSTpiHoxrRXXdspGlvQHbbQ45qM272")->get("https://kuin.summaict.nl/api/product");
+                $products = $products->json();
+
+                return redirect()->route('management.create', ['products' => $products, 'order_id' => $order["order_id"], "orderrows" => $order]);
+            }
+        }
+
+        return redirect()->route('management.create');
     }
 
     /**
