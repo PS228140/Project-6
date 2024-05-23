df3b071a3a8d11cea54921d03e1ff4c7e0bba2cb
Tomas.io <ps227258@SummaCollege.nl>
2024-05-23 17:33:55 +0200

    made the orderplacement complete

---
+++ webshop/groenevingers/resources/views/status.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Winkelwagen - GroeneVingers</title>
+    <link rel="shortcut icon" href="{{ url('assets/icons/favicon.svg') }}" type="image/x-icon">
+    <script src="{{ url('javascript/script.js') }}"></script>
+    <link rel="stylesheet" href="{{ url('css/includes/reset.css') }}">
+    <link rel="stylesheet" href="{{ url('css/includes/header.css') }}">
+    <link rel="stylesheet" href="{{ url('css/includes/footer.css') }}">
+    <link rel="stylesheet" href="{{ url('css/status.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" rel="stylesheet">
+</head>
+<body>
+    @include('includes.header')
+    <div class="wrapper">
+        <h2 class="title">Je bestelling</h2>
+
+    </div>
+    @include('includes.footer')
+</body>
+</html>
